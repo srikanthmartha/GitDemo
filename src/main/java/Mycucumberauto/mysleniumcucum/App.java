01b8414e75dd59d048009ad574a8d46ba0a0f5d5
srikanth <srikanthsriku2013@gmail.com>
2021-01-27 20:06:56 -0500

    architect changes in develop branch

--- src/main/java/Mycucumberauto/mysleniumcucum/App.java
+++ src/main/java/Mycucumberauto/mysleniumcucum/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "hi there" );
+        System.out.println( "how are you" );
     }
 }
